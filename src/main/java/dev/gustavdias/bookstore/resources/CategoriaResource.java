7a16a84d36c8f9a68687f800e2c6962c2f884df8
gustavodias <gustavoemersonfd@gmail.com>
2021-10-16 13:30:44 -0300

    utilizando padrao dto

--- src/main/java/dev/gustavdias/bookstore/resources/CategoriaResource.java
+++ src/main/java/dev/gustavdias/bookstore/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package dev.gustavdias.bookstore.resources;
 
 import dev.gustavdias.bookstore.domain.Categoria;
+import dev.gustavdias.bookstore.dtos.CategoriaDTO;
 import dev.gustavdias.bookstore.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,5 +24,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
         Categoria obj = categoriaService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = categoriaService.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
