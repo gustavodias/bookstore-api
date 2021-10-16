3abcb7104ea89ae0a35c53a337f832384957b804
gustavodias <gustavoemersonfd@gmail.com>
2021-10-16 00:23:02 -0300

    criando perfil de dev

--- src/main/java/dev/gustavdias/bookstore/BookstoreApplication.java
+++ src/main/java/dev/gustavdias/bookstore/BookstoreApplication.java
@@ -2,10 +2,14 @@ package dev.gustavdias.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class BookstoreApplication{
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
 }
