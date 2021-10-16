7e2146fc10562fc2cbd4a3a59cca2102b76bd669
gustavodias <gustavoemersonfd@gmail.com>
2021-10-16 18:21:17 -0300

    ajustes finalizados no backend

--- src/main/java/dev/gustavdias/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/dev/gustavdias/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import dev.gustavdias.bookstore.services.exceptions.DataIntegrityViolationExcept
 import dev.gustavdias.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -23,4 +25,18 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request){
+       ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+               "Erro na validacao dos campos");
+
+        for (FieldError x: e.getBindingResult().getFieldErrors()){
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+       
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+
 }
